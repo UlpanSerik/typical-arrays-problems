c118571b1122f85e96927948b7a0d6cd321166f0
UlpanSerik <bagitzanovaulpan@gmail.com>
2020-10-05 00:50:31 +0600

    fix: fix avg function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
+  if (array === undefined || array[0] === undefined){
   return 0;
+} else { let minV = array[0];
+  for (let i = 0; i<array.length; i++){
+    if (array[i]<minV)
+    {
+      minV = array[i];
+    }
+  }
+  return minV;
+}
 }
 
 exports.max = function max (array) {
+  if (array === undefined || array[0] === undefined){
   return 0;
-}
+} else {let maxV = array[0];
+  for (let i = 0; i<array.length; i++){
+    if (array[i]>maxV)
+    {
+      maxV = array[i];
+    }
+  }
+  return maxV;
+}}
 
 exports.avg = function avg (array) {
+  if (array === undefined || array[0] === undefined){
   return 0;
+   } let avgV = 0;
+  for (let i = 0; i<array.length; i++){
+    avgV += array[i];
+  }
+  return avgV / array.length;
 }
